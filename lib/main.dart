de092a0b48255bc29368683868f5ea413bd02a78
gnoparus <surapong@gmail.com>
2022-08-04 15:43:57 +0700

    Init import

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Widget titleSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(children: [
+      Expanded(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(bottom: 8),
+            child: const Text(
+              ' Oesshinen Lake Campground',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          const Text('Kandersteg, Switzerland',
+              style: TextStyle(color: Colors.grey))
+        ],
+      )),
+      const Icon(
+        Icons.star,
+        color: Colors.red,
+      ),
+      const Text('41')
+    ]),
+  );
+
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Welcome to Flutter'),
+          ),
+          body: Column(
+            children: [titleSection],
+          )),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+                fontSize: 12, fontWeight: FontWeight.w400, color: color),
+          ),
+        )
+      ],
+    );
+  }
+}
